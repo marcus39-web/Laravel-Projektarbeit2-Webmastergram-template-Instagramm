8226e53eaa2fe214c8a74865384a4de609b2148e
marcus39-web <marcus39-web@example.com>
2026-02-20 21:35:33 +0100

    Node läuft jetzt im Hintergrund

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
                 <h3 class="text-lg font-semibold mb-4">Neue Follower</h3>
                 @forelse($notifications as $notification)
